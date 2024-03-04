c180953799a5790285280d3be2185565789955b8
Oliver Billich <mlobillich1@sheffield.ac.uk>
2024-03-04 12:44:58 +0000

    add country list page to householder blueprint retrieve all countries from the database

--- solar_offset/views/householder.py
+++ solar_offset/views/householder.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, render_template
 
+from solar_offset.db import get_db
+
 bp = Blueprint("householder", __name__)
 
 @bp.route("/")
@@ -9,6 +11,16 @@ def home():
 @bp.route("/about")
 def about():
     return "Hello, About!"
+
+@bp.route("/countries")
+def country_list():
+    # TODO Make sure that the countries coming back are sorted (in some way)
+    # TODO Change SQL query to also include the number of donations and the total funds donated for each country
+    db = get_db()
+    countries = db.execute(
+        "SELECT * FROM country;"
+    ).fetchall()
+    return render_template("householder/country_list.html", countries=countries)
 
 @bp.route("/login")
 def login():
