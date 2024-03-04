ffbe02a99a5e7b9409f4917a996880366393ccfe
Oliver Billich <mlobillich1@sheffield.ac.uk>
2024-03-04 12:02:22 +0000

    add population sql file to add representative entries during development

---
+++ solar_offset/data-dev.sql
@@ -0,0 +1,9 @@
+
+-- Data to populate database during development for visualisation
+
+INSERT INTO country (country_code, short_code, name, solar_hours, carbon_emissions, solar_panel_price, electricity_mix_percentage, description)
+VALUES
+    -- Hours of Sunlight average estimated from https://www.currentresults.com/Weather/Spain/annual-days-of-sunshine.php (2445.702 hours)
+    -- Carbon Emissions for 2023 taken from https://app.electricitymaps.com/zone/ES (39.2 Megatons)
+    -- Price for Solar Panel taken from https://murciatoday.com/how-much-does-it-cost-to-install-solar-panels-in-spain_1672684-a.html (2000€/kW ~= £1800/kW)
+    ('ESP', 'ES', 'Spain', 2445, 39200000, 1800, 0.1617, 'Spain (España), or the Kingdom of Spain (Reino de España), is a country located in Southwestern Europe, with parts of its territory in the Atlantic Ocean, the Mediterranean Sea and Africa. It is the largest country in Southern Europe and the fourth-most populous European Union member state. [https://en.wikipedia.org/wiki/Spain]')
