95c598735bba937e3081f9c8b35fa72f399dc2fc
atharvakulkarni <askulkarni1@sheffield.ac.uk>
2024-03-05 11:23:03 +0000

    Added login register with db connecitivity

--- solar_offset/views/householder.py
+++ solar_offset/views/householder.py
@@ -1,18 +1,22 @@
-from flask import Blueprint, render_template,flash
-from solar_offset import get_db
+from flask import Flask, Blueprint, render_template, flash, request, session, redirect, url_for
 from werkzeug.security import check_password_hash, generate_password_hash
-
 from solar_offset.db import get_db
 
+app = Flask(__name__)
+app.secret_key = 'hkekekkwlbmvjtj'
 bp = Blueprint("householder", __name__)
+from uuid import uuid4
+
 
 @bp.route("/")
 def home():
     return render_template("householder/home.html")
 
+
 @bp.route("/about")
 def about():
     return "Hello, About!"
+
 
 @bp.route("/countries")
 def country_list():
@@ -27,11 +31,37 @@ def country_list():
     country_dicts = [dict(c) for c in countries]
     return render_template("householder/country_list.html", countries=country_dicts)
 
-@bp.route("/login")
+
+@bp.route("/login", methods=["GET", "POST"])
 def login():
+    session.clear()
+    if request.method == 'POST':
+        userid = request.form['userid']
+        password = request.form['password']
+        db = get_db()
+        error = None
+
+        # If the user is admin, we dont query the db and just check the username
+        if (userid == 'admin'):
+            flash("User login succesfull!", "success")
+            return render_template("./admin/admin.html", username=userid)
+        user = db.execute(
+            'SELECT * FROM householder WHERE display_name = ?', (userid,)
+        ).fetchone()
+        if user is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(user['password_hash'], password):
+            error = 'Incorrect password.'
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            flash("User login succesfull!", "success")
+            return render_template("./householder/householderdashboard.html", username=user["display_name"])
+        flash(error, "danger")
     return render_template("login.html")
 
-@bp.route("/register",methods=["GET","POST"])
+
+@bp.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == 'POST':
         userid = request.form['userid']
@@ -42,16 +72,16 @@ def register():
 
         try:
             db.execute(
-                "INSERT INTO user (username, password) VALUES (?, ?)",
-                (userid, generate_password_hash(password)),
+                "INSERT INTO householder (id,email,display_name,password_hash) VALUES (?,?,?,?)",
+                (str(uuid4()), email, userid, generate_password_hash(password)),
             )
             db.commit()
         except db.IntegrityError:
             error = f"User {userid} is already registered."
         else:
-            return redirect(url_for("login"))
+            flash("User registered succesfully!", "success")
+            return render_template("./admin/admin.html", username=userid)
 
-        flash(error)
+        print("Error", error)
 
-    return render_template('register.html')
-
+    return render_template('./register.html')
