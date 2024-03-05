314ad536c5eedb31636634f1ebcec747670d26f6
Oliver Billich <mlobillich1@sheffield.ac.uk>
2024-03-05 22:41:52 +0000

    add flag to return json response to country list

--- solar_offset/views/householder.py
+++ solar_offset/views/householder.py
@@ -1,5 +1,5 @@
 from math import floor
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 from solar_offset.db import get_db
 from solar_offset.util import calc_carbon_offset
@@ -21,16 +21,26 @@ def country_list():
         "SELECT country.*, COUNT(donation_amount) AS donation_count, SUM(donation_amount) AS donation_sum \
             FROM country LEFT JOIN donation \
             ON (country.country_code == donation.country_code) \
-            GROUP BY country.country_code;"
+            GROUP BY country.country_code \
+            ORDER BY country.name ASC;"
     ).fetchall()
 
     country_dicts = []
     for c_row in countries:
         cd = dict(c_row)
+        if not cd["donation_sum"]:
+            cd["donation_sum"] = 0
         cd["carbon_offset"] = floor(calc_carbon_offset(c_row))
         country_dicts.append(cd)
 
-    return render_template("householder/country_list.html", countries=country_dicts)
+    if "raw" in request.args:
+        for cd in country_dicts:
+            cd.pop("description")
+            cd.pop("electricty_consumption")
+            cd.pop("short_code")
+        return country_dicts
+    else:
+        return render_template("householder/country_list.html", countries=country_dicts)
 
 @bp.route("/login")
 def login():
