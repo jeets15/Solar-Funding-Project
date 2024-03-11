09f55c96f12f23c072f67ef7c8417a366bc7182a
atharvakulkarni <askulkarni1@sheffield.ac.uk>
2024-03-11 08:32:57 +0000

    Added admin table with delete functionality

--- solar_offset/views/admin.py
+++ solar_offset/views/admin.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template, request, session
+from flask import Blueprint, render_template, request, session, redirect
+from solar_offset.db import get_db
 
 bp = Blueprint("admin", __name__)
 
@@ -6,4 +7,17 @@ bp = Blueprint("admin", __name__)
 @bp.route("/admin", methods=["GET", "POST"])
 def admin():
     adminname = session.get('username')
-    return render_template("./admin/admin.html", adminname=adminname)
+    db = get_db()
+    users = db.execute(
+        'SELECT * FROM user WHERE user_type != ? ', ('admin',)
+    ).fetchall()
+    return render_template("./admin/admin.html", adminname=adminname, users=users)
+
+
+@bp.route('/delete_user', methods=['POST'])
+def delete_user():
+    user_id = request.form['user_id']
+    db = get_db()
+    db.execute("DELETE FROM user WHERE id=?", (user_id,))
+    db.commit()
+    return redirect('/admin')
