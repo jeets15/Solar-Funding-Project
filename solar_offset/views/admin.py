3aa011f382ba05d3f2298936162dbd9e9a4943fc
atharvakulkarni <askulkarni1@sheffield.ac.uk>
2024-03-11 13:13:39 +0000

    Added navbar and footer in admin dashboard

--- solar_offset/views/admin.py
+++ solar_offset/views/admin.py
@@ -6,13 +6,14 @@ bp = Blueprint("admin", __name__)
 
 @bp.route("/admin", methods=["GET", "POST"])
 def admin():
-    if request.method == "POST":
-        adminname = session.get('username')
-        db = get_db()
-        users = db.execute(
-            'SELECT * FROM user WHERE user_type != ? ', ('admin',)
-        ).fetchall()
-        return render_template("./admin/admin.html", adminname=adminname, users=users)
+    adminname = session.get('username')
+    db = get_db()
+    is_logged_in = True if adminname else False
+    print(is_logged_in)
+    users = db.execute(
+        'SELECT * FROM user WHERE user_type != ? ', ('admin',)
+    ).fetchall()
+    return render_template("./admin/admin.html", adminname=adminname, users=users, is_logged_in=is_logged_in)
 
 
 @bp.route('/delete_user', methods=['POST'])
