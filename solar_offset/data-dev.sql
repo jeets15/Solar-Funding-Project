c9c6c422aeb89a0443f83a0d46ddfd4aab17bdb7
Jeet Singhvi Pramod <jsinghvipramod1@sheffield.ac.uk>
2024-03-14 17:31:18 +0000

    Header footer - Base

--- solar_offset/data-dev.sql
+++ solar_offset/data-dev.sql
@@ -1,13 +1,12 @@
-
 -- Data to populate database during development for visualisation
 
-INSERT INTO country (country_code, short_code, name, solar_hours, carbon_emissions, solar_panel_price, electricity_mix_percentage, description)
+INSERT INTO country (country_code, short_code, name, solar_hours, carbon_emissions, solar_panel_price, electricity_mix_percentage, electricty_consumption, description)
 VALUES
     -- Hours of Sunlight average estimated from https://www.currentresults.com/Weather/Spain/annual-days-of-sunshine.php (2445.702 hours)
     -- Carbon Emissions for 2023 taken from https://app.electricitymaps.com/zone/ES (39.2 Megatons)
     -- Price for Solar Panel taken from https://murciatoday.com/how-much-does-it-cost-to-install-solar-panels-in-spain_1672684-a.html (2000€/kW ~= £1800/kW)
-    ('ESP', 'ES', 'Spain', 2445, 39200000, 1800, 0.1617, 'Spain (España), or the Kingdom of Spain (Reino de España), is a country located in Southwestern Europe, with parts of its territory in the Atlantic Ocean, the Mediterranean Sea and Africa. It is the largest country in Southern Europe and the fourth-most populous European Union member state. [https://en.wikipedia.org/wiki/Spain]');
-
+    ('ESP', 'ES', 'Spain', 2445, 39200000, 1800, 0.1617, 259,
+     'Spain (España), or the Kingdom of Spain (Reino de España), is a country located in Southwestern Europe, with parts of its territory in the Atlantic Ocean, the Mediterranean Sea and Africa. It is the largest country in Southern Europe and the fourth-most populous European Union member state. [https://en.wikipedia.org/wiki/Spain]');
 
 INSERT INTO user (id, email_username, password_hash, user_type)
 VALUES ("8b1a1136-0024-477f-9e29-cb7266cb46d6", "admin1@12",
@@ -18,8 +17,6 @@ INSERT INTO user (id, email_username, password_hash, user_type)
 VALUES ("ed8425f7-8313-4382-9d7d-2b061eb890c5", "staff3881@hhrs",
         "scrypt:32768:8:1$oXJ7HMTqbUOnRQ51$4f70127d583052d1195fed4d00a1ba879b44d4c203173d2fa890d1f2f217ad5abc398ff31dac9415d51c4beb5035a5ad8034fce57afb3820a0d28c71b0f89b51",
         "_s_"); -- Password = "staff@29r83910"
-
-
 
 INSERT INTO projects (name, description, sites, status, country_code)
 VALUES
@@ -32,3 +29,4 @@ VALUES
 INSERT INTO countryinfo (description,country_code)
 VALUES
     ('Solar power in Spain has grown significantly due to favorable conditions and government support. Abundant sunshine, coupled with investments in solar technology, has led to increased capacity. There''s still potential for further growth, supported by ongoing advancements in technology and favorable policies aimed at increasing renewable energy usage.','ESP');
+
--- solar_offset/schema.sql
+++ solar_offset/schema.sql
@@ -1,32 +1,40 @@
-
--- Drop all tables to reset DB
-DROP TABLE IF EXISTS householder;
-DROP TABLE IF EXISTS staff;
-DROP TABLE IF EXISTS administrator;
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS user_status;
 DROP TABLE IF EXISTS country;
 DROP TABLE IF EXISTS organization;
 DROP TABLE IF EXISTS donation;
-DROP TABLE IF EXISTS projects;
-DROP TABLE IF EXISTS countryinfo;
 
 
-CREATE TABLE householder (
-    id CHAR(36) PRIMARY KEY, -- Entries to the id column will be generated using uuid.uuid4(), a random 36 character string
-    email VARCHAR(70) UNIQUE NOT NULL, -- 70 as an upper limit for e-mail length is already very long
-    display_name VARCHAR(70) NOT NULL, -- Display name is how the householder will be adressed
-    password_hash TEXT NOT NULL -- Password hashes will be generated using werkzeug.security.generate_password_hash and checked with werkzeug.security.check_password_hash
+CREATE TABLE user (
+    -- id: A unique id for each user; this value should only be used in the back-end or as a session cookie
+    -- Users should normally not have access to this id
+    -- Entries to the id column will be generated using
+    --   uuid.uuid4(), a random 36 character string
+    id CHAR(36) CHECK( LENGTH(id) == 36 ) PRIMARY KEY,
+    -- email_username: The e-mail address of a registered householder
+    -- internal account types like admin and staff will usually have a username instead of an e-mail
+    -- this column must be unique as users will use e-mail/username to log in to solar offset
+    -- 70 as an upper limit is already very long
+    email_username VARCHAR(70) CHECK( LENGTH(id) <= 70 ) UNIQUE NOT NULL,
+    -- password_hash: The hashed password that the login will be compared against
+    -- Password hashes will be generated using werkzeug.security.generate_password_hash
+    --   and checked with werkzeug.security.check_password_hash
+    password_hash TEXT NOT NULL,
+    -- display_name: optional entry that will be shown on personalised pages
+    -- usually only used for the householder user type
+    display_name VARCHAR(70) CHECK( LENGTH(id) <= 70 ),
+    -- user_type: defines which type of user this entry belongs to
+    -- h ~ householder, s ~ staff, a ~ admin
+    -- if a user is a householder, they can't have any other roles
+    -- a user can be admin, staff, or both at the same time
+    user_type CHAR(3) CHECK( user_type IN ('h__', '__a', '_s_', '_sa') )
 );
 
-CREATE TABLE staff (
-     id CHAR(36) PRIMARY KEY, -- Entries to the id column will be generated using uuid.uuid4(), a random 36 character string
-     username VARCHAR(70) UNIQUE NOT NULL, -- Unique username that staff will use to log in
-     password_hash TEXT NOT NULL -- Password hashes will be generated using werkzeug.security.generate_password_hash and checked with werkzeug.security.check_password_hash
-);
-
-CREATE TABLE administrator (
-     id CHAR(36) PRIMARY KEY, -- Entries to the id column will be generated using uuid.uuid4(), a random 36 character string
-     username VARCHAR(70) UNIQUE NOT NULL, -- Unique username that administrators will use to log in
-     password_hash TEXT NOT NULL -- Password hashes will be generated using werkzeug.security.generate_password_hash and checked with werkzeug.security.check_password_hash
+CREATE TABLE user_status (
+    user_id CHAR(36) CHECK( LENGTH(user_id) == 36 ) PRIMARY KEY, -- Foreign key reference to user table
+    flag_suspicious BOOLEAN NOT NULL CHECK( flag_suspicious IN (0, 1) ), -- 0 (false) if not suspicious, 1 (true) if suspicious
+    suspend TEXT DEFAULT NULL, -- If user is suspended set column to a string containing the "suspension message"
+    FOREIGN KEY (user_id) REFERENCES user (id)
 );
 
 CREATE TABLE country (
@@ -36,8 +44,9 @@ CREATE TABLE country (
     description TEXT, -- long-ish description about country in relation to solar offset
     solar_hours INTEGER, -- hours of sunlight per year (estimate)
     carbon_emissions INTEGER, -- Yearly carbon emissions (from electricity production ideally) in Tons of CO2
-    solar_panel_price DOUBLE, -- Price for constructing a solar panel in (£ / kW)
-    electricity_mix_percentage DOUBLE -- What percentage of the electricity mix is generated from solar energy
+    solar_panel_price INTEGER, -- Price for constructing a solar panel in (£ / kW)
+    electricity_mix_percentage DOUBLE, -- What percentage of the electricity mix is generated from solar energy
+    electricty_consumption INTEGER -- Electric Energy being used in the Country (TWh)
 );
 
 CREATE TABLE organization (
@@ -58,7 +67,7 @@ CREATE TABLE donation (
     organization_slug TEXT, -- Foreign key composite with country code
     donation_amount INTEGER, -- Set the rule that only whole numbers are allowed as donations
     PRIMARY KEY (created, householder_id, country_code, organization_slug),
-    FOREIGN KEY (householder_id) REFERENCES householder (id),
+    FOREIGN KEY (householder_id) REFERENCES user (id),
     FOREIGN KEY (country_code) REFERENCES organization (country_code),
     FOREIGN KEY (organization_slug) REFERENCES organization (name_slug)
 );
