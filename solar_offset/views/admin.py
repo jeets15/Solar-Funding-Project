5334087838849fbc808d4af4551f0bd6356e2d1f
atharvakulkarni <askulkarni1@sheffield.ac.uk>
2024-03-16 14:17:03 +0000

    Added unflag functionality for admin

--- solar_offset/views/admin.py
+++ solar_offset/views/admin.py
@@ -16,6 +16,15 @@ def admin():
     users = db.execute(
         'SELECT * FROM user WHERE user_type NOT LIKE ? ', ('%a%',)
     ).fetchall()
+    user_status_list = db.execute('SELECT user_id,flag_suspicious FROM user_status WHERE flag_suspicious=?',
+                                  (1,)).fetchall()
+
+    user_status_dict = []
+    for user_row in user_status_list:
+        userstatusdict = dict(user_row)
+        user_status_dict.append(userstatusdict)
+
+    print(user_status_dict)
     user_dicts = []
     for user_row in users:
         userdict = dict(user_row)
@@ -41,9 +50,30 @@ def delete_user():
 def flag_user():
     user_id = request.form['user_id']
     db = get_db()
+    users = db.execute(
+        'SELECT * FROM user_status WHERE user_id = ? ', (user_id,)
+    ).fetchone()
+    if users is None:
+        db.execute(
+            "INSERT INTO user_status (user_id, flag_suspicious) VALUES (?,?)",
+            (user_id, 1),
+        )
+    else:
+        db.execute(
+            "UPDATE user_status SET flag_suspicious = ? WHERE user_id = ?",
+            (1, user_id),
+        )
+    db.commit()
+    return redirect('/admin')
+
+
+@bp.route('/unflag-user', methods=['POST'])
+def unflag_user():
+    user_id = request.form['user_id']
+    db = get_db()
     db.execute(
-        "INSERT INTO user_status (user_id, flag_suspicious) VALUES (?,?)",
-        (user_id, 1),
+        "UPDATE user_status SET flag_suspicious = ? WHERE user_id = ?",
+        (0, user_id),
     )
     db.commit()
     return redirect('/admin')
