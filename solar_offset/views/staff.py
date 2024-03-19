1c1ab0e286587a6c4f44afa221e96cabdeb15a54
Vivek Shravan Tate <vivektate.developer@gmail.com>
2024-03-19 00:41:47 +0000

    Fixed code for staff dashboard report.

--- solar_offset/views/staff.py
+++ solar_offset/views/staff.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, session
+from flask import Blueprint, render_template, request, session, redirect
 from solar_offset.db import get_db
 
 from math import floor
@@ -22,21 +22,26 @@ def staff():
                            is_logged_in=is_logged_in)
 
 
-@bp.route("/staff/report")
+@bp.route("/staff/report/", methods=["POST"])
 def report():
-    staffname = session.get('username')
+    staff_name = session.get('username')
     db = get_db()
-    is_logged_in = True if staffname else False
-    users = db.execute(
-        "SELECT u.email_username AS name, c.name AS country_name, o.name AS organization_name, SUM(d.donation_amount) AS total_donation_amount \
-            FROM user u \
-            JOIN donation d ON u.id = d.householder_id \
-            JOIN organization o ON d.country_code = o.country_code AND d.organization_slug = o.name_slug \
-            JOIN country c ON d.country_code = c.country_code \
-            GROUP BY c.name, o.name;"
-    ).fetchall()
+    is_logged_in = True if staff_name else False
+    if not is_logged_in:
+        return redirect("/login")
+
+    country = request.form['country']
+    organization = request.form['organization']
+
+    query = """
+        SELECT u.email_username AS name, c.name AS country_name, o.name AS organization_name, SUM(d.donation_amount) AS total_donation_amount
+        FROM user u
+        JOIN donation d ON u.id = d.householder_id
+        JOIN country c ON c.country_code = d.country_code
+        JOIN organization o ON d.country_code = o.country_code AND d.organization_slug = o.name_slug
+        WHERE c.name = ? AND o.name = ?
+        GROUP BY u.id, c.name, o.name;
+        """
+    users = db.execute(query, (country, organization)).fetchall()
 
     return render_template("./staff/report.html", users=users, is_logged_in=is_logged_in)
-    if is_logged_in == False:
-        return redirect("/login")
-    return render_template("./users/staff/staffdashboard.html", staffname=staffname, is_logged_in=is_logged_in)
