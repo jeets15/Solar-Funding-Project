6f3ebecf127f74d0c513339b81c2bd4ecbc24724
Oliver Billich <mlobillich1@sheffield.ac.uk>
2024-03-24 16:45:52 +0000

    add utility function for significant figure rounding new functions rounds a given input number to the given number of significant figures

--- solar_offset/utils/misc.py
+++ solar_offset/utils/misc.py
@@ -1,6 +1,6 @@
 
 from collections.abc import Iterable
-from math import isclose
+from math import floor, isclose, log10
 
 # Find the nth percentile by interpolating between the given data points
 def calculate_percentile(finite_iter:Iterable[int|float], percentile:float):
@@ -21,3 +21,10 @@ def calculate_percentile(finite_iter:Iterable[int|float], percentile:float):
         r1 = ranked_list[rank_ix_int]
         r2 = ranked_list[rank_ix_int+1]
         return min(r1, r2) + abs( (r2-r1) * rank_ix_remainder )
+
+
+## Code taken from https://stackoverflow.com/a/3411435
+##   Comment authored by user Roy Hyunjin Han (https://stackoverflow.com/users/192092/roy-hyunjin-han)
+def round_to_n_sig_figs(num:int|float, significant_figures):
+    round_ix = - int( floor(log10(num)) ) + (significant_figures - 1)
+    return round(num, round_ix)
