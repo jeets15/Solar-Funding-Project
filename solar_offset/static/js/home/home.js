39e69e0bb54e68fe28bd7a6a992d316b75c1e717
Vivek Shravan Tate <vivektate.developer@gmail.com>
2024-04-02 20:21:39 +0100

    Fixed the issue of map resizing & button re-positioning on map.

--- solar_offset/static/js/home/home.js
+++ solar_offset/static/js/home/home.js
@@ -18,7 +18,6 @@ drawMap(document.getElementById("canvas-3"));
 
 // Function to draw map on canvas
 function drawMap(canvas) {
-
     // Customize the dimensions of canvas
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
@@ -30,51 +29,59 @@ function drawMap(canvas) {
 
     // Event Handler on image load
     img.onload = function () {
-
         // Draw the image on the canvas
         ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
 
-        // South Africa
-        const south_africa = createButton("../../static/images/countries/south-africa.png", 520, 290, "https://www.google.com/");
-        canvas.parentNode.appendChild(south_africa);
+        // Remove any existing buttons
+        const buttons = canvas.parentNode.querySelectorAll("button");
+        buttons.forEach(button => button.remove());
 
-        // Argentina
-        const argentina = createButton("../../static/images/countries/argentina.png", 300, 300, "https://www.google.com/");
-        canvas.parentNode.appendChild(argentina);
-
-        // Australia
-        const australia = createButton("../../static/images/countries/australia.png", 775, 280, "https://www.google.com/");
-        canvas.parentNode.appendChild(australia);
-
-        // Brazil
-        const brazil = createButton("../../static/images/countries/brazil.png", 330, 250, "https://www.google.com/");
-        canvas.parentNode.appendChild(brazil);
-
-        // Canada
-        const canada = createButton("../../static/images/countries/canada.png", 200, 85, "https://www.google.com/");
-        canvas.parentNode.appendChild(canada);
-
-        // Iran
-        const iran = createButton("../../static/images/countries/iran.png", 590, 150, "https://www.google.com/");
-        canvas.parentNode.appendChild(iran);
-
-        // Saudi Arabia
-        const saudi_arabia = createButton("../../static/images/countries/saudi-arabia.png", 570, 170, "https://www.google.com/");
-        canvas.parentNode.appendChild(saudi_arabia);
-
-        // USA
-        const usa = createButton("../../static/images/countries/united-states-of-america.png", 200, 130, "https://www.google.com/");
-        canvas.parentNode.appendChild(usa);
-
+        // Add the buttons again with updated positions and sizes
+        addButtons(canvas);
     };
+}
+
+// Function to add buttons on the map
+function addButtons(canvas) {
+    // South Africa
+    const south_africa = createButton("../../static/images/countries/south-africa.png", 55, 57, "https://www.google.com/");
+    canvas.parentNode.appendChild(south_africa);
+
+    // Argentina
+    const argentina = createButton("../../static/images/countries/argentina.png", 32, 60, "https://www.google.com/");
+    canvas.parentNode.appendChild(argentina);
+
+    // Australia
+    const australia = createButton("../../static/images/countries/australia.png", 84, 55, "https://www.google.com/");
+    canvas.parentNode.appendChild(australia);
+
+    // Brazil
+    const brazil = createButton("../../static/images/countries/brazil.png", 35, 50, "https://www.google.com/");
+    canvas.parentNode.appendChild(brazil);
+
+    // Canada
+    const canada = createButton("../../static/images/countries/canada.png", 20, 17, "https://www.google.com/");
+    canvas.parentNode.appendChild(canada);
+
+    // Iran
+    const iran = createButton("../../static/images/countries/iran.png",63 , 29, "https://www.google.com/");
+    canvas.parentNode.appendChild(iran);
+
+    // Saudi Arabia
+    const saudi_arabia = createButton("../../static/images/countries/saudi-arabia.png", 60, 33, "https://www.google.com/");
+    canvas.parentNode.appendChild(saudi_arabia);
+
+    // USA
+    const usa = createButton("../../static/images/countries/united-states-of-america.png", 20, 25, "https://www.google.com/");
+    canvas.parentNode.appendChild(usa);
 }
 
 // Function to create button on the map
 function createButton(imageUrl, x, y, url) {
     const button = document.createElement("button");
     button.style.position = "absolute";
-    button.style.top = y + "px";
-    button.style.left = x + "px";
+    button.style.top = y + "%";
+    button.style.left = x + "%";
     button.style.width = "25px";
     button.style.height = "25px";
     button.style.border = "none";
@@ -87,6 +94,14 @@ function createButton(imageUrl, x, y, url) {
     });
     return button;
 }
+
+
+// Add event listener for window resize
+window.addEventListener("resize", function () {
+    const canvases = document.querySelectorAll("#canvas-1, #canvas-2, #canvas-3");
+    canvases.forEach(canvas => drawMap(canvas));
+});
+
 
 
 
