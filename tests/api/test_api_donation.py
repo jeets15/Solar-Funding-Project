4ee99a8c159251309b230e50957469aaee0e9df5
atharvakulkarni <askulkarni1@sheffield.ac.uk>
2024-04-06 19:54:21 +0100

    Updated the testcases for donation

--- tests/api/test_api_donation.py
+++ tests/api/test_api_donation.py
@@ -1,5 +1,7 @@
 from flask import session, url_for
 from solar_offset.db import get_db
+import json
+
 
 # TODO add more unit tests
 
@@ -39,93 +41,125 @@ def test_donate_get(client):
     assert res.status_code == 200
 
 
-
 def test_donate_post_not_logged_in(client):
-    res = client.post("/api/donate", data={})
+    headers = {'Content-Type': 'application/json'}
+    res = client.post("/api/donate", data=json.dumps({}), headers=headers)
     assert res.status_code == 401
     assert res.text == "You must be logged in to donate"
-
 
 
 def test_donate_post_modified_user_session(client):
     with client.session_transaction() as session:
         session['user_id'] = '123456789'
-    res = client.post("/api/donate", data={})
+    headers = {'Content-Type': 'application/json'}
+    res = client.post("/api/donate", data=json.dumps({}), headers=headers)
     assert (res.status_code, res.text) == (400, "Invalid User Credentials")
-
 
 
 def test_donate_post_logged_in_non_householder(client, auth):
     with client:
         auth.login(username="staff3881@hhrs", password="staff@29r83910")
-        res = client.post("/api/donate", data={})
+        headers = {'Content-Type': 'application/json'}
+        res = client.post("/api/donate", data=json.dumps({}), headers=headers)
         assert (res.status_code, res.text) == (403, "Only Householder accounts are allowed to donate")
 
     with client:
         auth.login(username="admin1@12", password="admin$219047")
-        res = client.post("/api/donate", data={})
+        headers = {'Content-Type': 'application/json'}
+        res = client.post("/api/donate", data=json.dumps({}), headers=headers)
         assert (res.status_code, res.text) == (403, "Only Householder accounts are allowed to donate")
 
     with client:
         auth.login(username="staffmin12", password="staffmin12")
-        res = client.post("/api/donate", data={})
+        headers = {'Content-Type': 'application/json'}
+        res = client.post("/api/donate", data=json.dumps({}), headers=headers)
         assert (res.status_code, res.text) == (403, "Only Householder accounts are allowed to donate")
-
 
 
 def test_donate_post_logged_in_householder_no_data(client, auth):
     with client:
         auth.login(username="jane.doe15@example.com", password="12Jane!DoeDoe")
-        res = client.post("/api/donate", data={})
+        headers = {'Content-Type': 'application/json'}
+        res = client.post("/api/donate", data=json.dumps({}), headers=headers)
         assert (res.status_code, res.text) == (400, "You must supply a 'country_code' entry") \
-            or (res.status_code, res.text) == (400, "You must supply a 'organization_slug' entry") \
-            or (res.status_code, res.text) == (400, "You must supply a 'donation_amount' entry")
-
+               or (res.status_code, res.text) == (400, "You must supply a 'organization_slug' entry") \
+               or (res.status_code, res.text) == (400, "You must supply a 'donation_amount' entry")
 
 
 def test_donate_post_logged_in_householder_single_data(client, auth):
     with client:
         auth.login(username="jane.doe15@example.com", password="12Jane!DoeDoe")
-        res = client.post("/api/donate", data={"country_code": "foo"})
+        data = {
+            "country_code": "foo"
+        }
+        json_data = json.dumps(data)
+        headers = {'Content-Type': 'application/json'}
+        res = client.post("/api/donate", data=json_data, headers=headers)
         assert (res.status_code, res.text) == (400, "You must supply a 'organization_slug' entry") \
-            or (res.status_code, res.text) == (400, "You must supply a 'donation_amount' entry")
+               or (res.status_code, res.text) == (400, "You must supply a 'donation_amount' entry")
 
     with client:
         auth.login(username="jane.doe15@example.com", password="12Jane!DoeDoe")
-        res = client.post("/api/donate", data={"organization_slug": "bar"})
+        data = {
+            "organization_slug": "bar"
+        }
+        json_data = json.dumps(data)
+        headers = {'Content-Type': 'application/json'}
+        res = client.post("/api/donate", data=json_data, headers=headers)
         assert (res.status_code, res.text) == (400, "You must supply a 'country_code' entry") \
-            or (res.status_code, res.text) == (400, "You must supply a 'donation_amount' entry")
-        
+               or (res.status_code, res.text) == (400, "You must supply a 'donation_amount' entry")
+
     with client:
         auth.login(username="jane.doe15@example.com", password="12Jane!DoeDoe")
-        res = client.post("/api/donate", data={"donation_amount": "NaN"})
+        data = {
+            "donation_amount": "NaN"
+        }
+        json_data = json.dumps(data)
+        headers = {'Content-Type': 'application/json'}
+        res = client.post("/api/donate", data=json_data, headers=headers)
         assert (res.status_code, res.text) == (400, "You must supply a 'country_code' entry") \
-            or (res.status_code, res.text) == (400, "You must supply a 'organization_slug' entry")
-        
+               or (res.status_code, res.text) == (400, "You must supply a 'organization_slug' entry")
 
 
 def test_donate_post_logged_in_householder_data_pairs(client, auth):
     with client:
         auth.login(username="jane.doe15@example.com", password="12Jane!DoeDoe")
-        res = client.post("/api/donate", data={"country_code": "foo", "organization_slug": "bar"})
+        data = {
+            "country_code": "foo",
+            "organization_slug": "bar"
+        }
+        json_data = json.dumps(data)
+        headers = {'Content-Type': 'application/json'}
+        res = client.post("/api/donate", data=json_data, headers=headers)
         assert (res.status_code, res.text) == (400, "You must supply a 'donation_amount' entry")
-        
+
     with client:
         auth.login(username="jane.doe15@example.com", password="12Jane!DoeDoe")
-        res = client.post("/api/donate", data={"country_code": "foo", "donation_amount": "NaN"})
+        data = {
+            "country_code": "foo", "donation_amount": "NaN"
+        }
+        json_data = json.dumps(data)
+        headers = {'Content-Type': 'application/json'}
+        res = client.post("/api/donate", data=json_data, headers=headers)
         assert (res.status_code, res.text) == (400, "You must supply a 'organization_slug' entry")
-        
+
     with client:
         auth.login(username="jane.doe15@example.com", password="12Jane!DoeDoe")
-        res = client.post("/api/donate", data={"organization_slug": "bar", "donation_amount": "NaN"})
+        data = {
+            "organization_slug": "bar", "donation_amount": "NaN"
+        }
+        json_data = json.dumps(data)
+        headers = {'Content-Type': 'application/json'}
+        res = client.post("/api/donate", data=json_data, headers=headers)
         assert (res.status_code, res.text) == (400, "You must supply a 'country_code' entry")
 
-    
+
 def test_donate_post_logged_in_householder_full_data(client, auth):
     cases = [
         (
             {"country_code": "foo", "organization_slug": "bar", "donation_amount": "NaN"},
-            [(400, "Specified Country does not Exist"), (400, "Specified Organization does not Exist"), (400, "Donation Amount must be a positive integer")]
+            [(400, "Specified Country does not Exist"), (400, "Specified Organization does not Exist"),
+             (400, "Donation Amount must be a positive integer")]
         ),
         (
             {"country_code": "ata", "organization_slug": "bar", "donation_amount": "NaN"},
@@ -159,44 +193,57 @@ def test_donate_post_logged_in_householder_full_data(client, auth):
     for post_data, assertions in cases:
         with client:
             auth.login(username="jane.doe15@example.com", password="12Jane!DoeDoe")
-            res = client.post("/api/donate", data=post_data)
+            res = client.post("/api/donate", data=json.dumps(post_data), content_type="application/json")
             print((res.status_code, res.text))
             print(assertions)
-            assert any([ (res.status_code, res.text) == a for a in assertions ])
+            assert any([(res.status_code, res.text) == a for a in assertions])
+
 
 def test_donate_post_donation_amount(client, auth):
     with client:
         auth.login(username="jane.doe15@example.com", password="12Jane!DoeDoe")
-        res = client.post("/api/donate", data={
+        data = {
             "country_code": "ata",
             "organization_slug": "antarctica_solar_project",
             "donation_amount": "1.1"
-        })
+        }
+        json_data = json.dumps(data)
+
+        headers = {'Content-Type': 'application/json'}
+        res = client.post("/api/donate", data=json_data, headers=headers)
         assert (res.status_code, res.text) == (400, "Donation Amount must be a positive integer")
-    
+
     with client:
         auth.login(username="jane.doe15@example.com", password="12Jane!DoeDoe")
-        res = client.post("/api/donate", data={
+        data = {
             "country_code": "ata",
             "organization_slug": "antarctica_solar_project",
             "donation_amount": "0"
-        })
+        }
+        json_data = json.dumps(data)
+        headers = {'Content-Type': 'application/json'}
+        res = client.post("/api/donate", data=json_data, headers=headers)
         assert (res.status_code, res.text) == (400, "Donation Amount must be a positive integer")
 
     with client:
         auth.login(username="jane.doe15@example.com", password="12Jane!DoeDoe")
-        res = client.post("/api/donate", data={
+        data = {
             "country_code": "ata",
             "organization_slug": "antarctica_solar_project",
             "donation_amount": "0.5"
-        })
+        }
+        json_data = json.dumps(data)
+        headers = {'Content-Type': 'application/json'}
+        res = client.post("/api/donate", data=json_data, headers=headers)
         assert (res.status_code, res.text) == (400, "Donation Amount must be a positive integer")
 
     with client:
         auth.login(username="jane.doe15@example.com", password="12Jane!DoeDoe")
-        res = client.post("/api/donate", data={
+        data = {
             "country_code": "ata",
             "organization_slug": "antarctica_solar_project",
             "donation_amount": "-1"
-        })
+        }
+        json_data = json.dumps(data)
+        res = client.post("/api/donate", data=json_data, headers=headers)
         assert (res.status_code, res.text) == (400, "Donation Amount must be a positive integer")
