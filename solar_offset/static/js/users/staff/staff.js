dcf16c8add757234a47c03678257b383f90f7529
Oliver Billich <mlobillich1@sheffield.ac.uk>
2024-04-25 21:26:53 +0100

    clean up unnecessary code

--- solar_offset/static/js/users/staff/staff.js
+++
@@ -1,6 +0,0 @@
-setTimeout(function () {
-    var flashMessages = document.getElementById('flash-messages');
-    if (flashMessages) {
-        flashMessages.style.display = 'none';
-    }
-}, 5000);
